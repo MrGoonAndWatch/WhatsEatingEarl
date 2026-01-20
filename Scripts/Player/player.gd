5177a2ba8b9bfeabbbdffb881a7c094c56c37c51
Goon <mrgoonandwatch@gmail.com>
2026-01-19 19:50:58 -0500

    Wire up look logic to controller/keyboard.

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -8,20 +8,26 @@ const VERT_LOOK_CAP_DEG: float = 70
 
 # TODO: MOVE THESE IN TO A CONFIG DATA STRUCT!!!
 const MOUSE_SENSITIVITY: float = 0.01
-const CONTROLLER_SENSITIVITY: float = 10
+const GAMEPAD_LOOK_SENSITIVITY: float = 10
 
 func _physics_process(delta: float) -> void:
 	velocity = get_movement(delta)
 	process_look(delta)
-	
 	move_and_slide()
 
 func _input(event):
-	if event is InputEventMouseMotion:
-		print("mouse capture x=%s, y=%s" % [event.relative.x, event.relative.y])
-		rotate_camera(event.relative.x * MOUSE_SENSITIVITY, event.relative.y * MOUSE_SENSITIVITY)
-	# elif event is InputEventJoypadMotion:
+	if event is InputEventMouseMotion and event.relative != Vector2.ZERO:
+		# print("mouse capture %s" % [event.relative])
+		var mouse_dir = event.relative * MOUSE_SENSITIVITY
+		rotate_camera(mouse_dir)
+	# This works but ew.
+	# elif (event is InputEventJoypadMotion 
+	#	and abs(event.axis_value) > GAMEPAD_DEADZONE):
 	#	print ("joypad motion captured %s" % [event])
+	#	if event.axis == RSTICK_H_AXISID: # 2
+	#		rotate_camera(event.axis_value, 0)
+	#	elif event.axis == RSTICK_V_AXISID: # 3
+	#		rotate_camera(0, event.axis_value)
 
 func get_movement(delta: float) -> Vector3:
 	var move_input = wee_constants.get_movement_vector()
@@ -32,15 +38,19 @@ func get_movement(delta: float) -> Vector3:
 	var move_input_total = Vector3(move_input.x, 0, move_input.y) * delta * player_speed * run_mult
 	return move_input_total.rotated(Vector3.UP, rotating_parts.global_rotation.y)
 
+# Note: Mouse look is handled by _input(), this only handles keyboard/gamepad input.
 func process_look(delta: float) -> void:
 	var look_input = wee_constants.get_look_vector()
 	if look_input == Vector2.ZERO:
 		return
+	
+	look_input *= delta * GAMEPAD_LOOK_SENSITIVITY
+	rotate_camera(look_input)
 
-func rotate_camera(x: float, y: float) -> void:
-	if x == 0 && y == 0:
+func rotate_camera(dir: Vector2) -> void:
+	if dir == Vector2.ZERO:
 		return
 	
-	rotate_y(-x)
-	rotating_parts.rotate_x(-y)
+	rotate_y(-dir.x)
+	rotating_parts.rotate_x(-dir.y)
 	rotating_parts.rotation.x = clampf(rotating_parts.rotation.x, -deg_to_rad(VERT_LOOK_CAP_DEG), deg_to_rad(VERT_LOOK_CAP_DEG))
--- Scripts/wee_constants.gd
+++ Scripts/wee_constants.gd
@@ -12,11 +12,26 @@ class controls:
 	const look_right: String = "look_right"
 	const run: String = "run"
 
+# Could be a config value, though piping it through to the static methods in this class might be jank.
+const GAMEPAD_DEADZONE: float = 0.1
+
 static func get_movement_vector() -> Vector2:
-	return Input.get_vector(controls.strafe_left, controls.strafe_right, controls.move_forwards, controls.move_backwards)
+	var move_vector = Input.get_vector(controls.strafe_left, controls.strafe_right, controls.move_forwards, controls.move_backwards)
+	return apply_deadzone(move_vector, GAMEPAD_DEADZONE)
 
 ###
-# ONLY WORKS FOR CONTROLLER LOOK AND KEYBOARD LOOK... IDK WHY BUT I CAN'T BIND MOUSE MOVEMENTS TO INPUTS IN GODOT!!!
+# ONLY WORKS FOR CONTROLLER LOOK AND KEYBOARD LOOK... 
+# IDK WHY BUT I CAN'T BIND MOUSE MOVEMENTS TO INPUTS IN GODOT!!!
 ###
 static func get_look_vector() -> Vector2:
-	return Input.get_vector(controls.look_left, controls.look_right, controls.look_up, controls.look_down)
+	var look_vector = Input.get_vector(controls.look_left, controls.look_right, controls.look_up, controls.look_down)
+	return apply_deadzone(look_vector, GAMEPAD_DEADZONE)
+
+static func apply_deadzone(vec: Vector2, deadzone: float) -> Vector2:
+	var x = vec.x
+	var y = vec.y
+	if abs(x) < deadzone:
+		x = 0
+	if abs(vec.y) < deadzone:
+		y = 0
+	return Vector2(x, y)
